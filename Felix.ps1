8629836996c881fb499587f85639a68fd330a475
FireLies <jeantitor@protonmail.com>
2022-03-18 08:51:18 +0700

    Removed colored output

--- Felix.ps1
+++ Felix.ps1
@@ -1,25 +1,9 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
-function Write-Color {
-
-    param (
-        [Parameter()][string]$Text,
-        [Parameter(Mandatory=$false)][string]$Color = 'White'
-    )
-
-    $TextColor = @{
-        Object = $Text;
-        ForegroundColor = $Color;
-        NoNewline = $true
-    }
-
-    Write-Host @TextColor
-
-}
-
-Write-Color "`nHello Felix! -| v1.6 |- FireLies 2022`n" 
-Write-Color "`nUsage:" -Color Cyan; Write-Color " felix [-Extension] [-Path]`n`n"
-""
+@"
+`nHello Felix! -| v1.7 |- FireLies 2022 -> https://github.com/FireLies/Felix
+`nUsage: felix [-Extension] [-Path]`n
+"@
 
 function Felix {
 
@@ -29,16 +13,16 @@ function Felix {
         [Parameter(Mandatory=$true)]
         [ValidateScript({
 
-            $ExtSet = '.docx', '.doc', '.pdf', '.pptx', '.xlsx', '.xls', '.txt', '.mp4', '.mp3', '.jpg', '.jpeg', '.png', '*'
-
-            if (($_ -notin $ExtSet) -or (!($_))) {
-                Write-Color "`nPlease assign the extension parameter correctly `nValid extensions: "; Write-Color "$($ExtSet -join ', ')`n" -Color 'Cyan'
-                Write-Color "`nTIPS:" -Color 'Cyan'; Write-Color " --Use felix * to select all extensions `n      --Use separator (,) to assign more than 1`n`n"
-                $false; break
+            if (($_ -notin ([string[]]$ValidExtensions = Get-Content $PWD\ValidExtensions.txt)) -or (!($_))) {
+                Write-Host "
+                `rPlease assign the extension parameter correctly `nValid extensions: $($ValidExtensions -join ', ')
+                `nTIPS: --Use felix * to select all extensions
+                `r      --Use (,) to assign more than 1`n"
+                $false
+                break
             }
 
             $true
-
         })]
         [string[]]$Extension,
 
@@ -46,35 +30,34 @@ function Felix {
         [string]$Path = "C:\Users\"+[Environment]::UserName+"\Documents"
     )
 
-    ""
-    if (!(Test-Path "$Path")) {
-        Write-Color "$Path does not exist. Please check the name correctly`n"
-        ""; break
-    }
+    
+    process {
 
-    Write-Color "The following proccess will be done recursively. You're on your own`n" -Color 'Cyan'
-    cmd /c Pause
+        if (!(Test-Path "$Path")) {
+            Write-Host "$Path does not exist. Please check the name correctly`n"
+            break
+        }
 
-    ""
-    foreach ($Value in $Extension) {
+        Write-Host "`nThe following process will be done recursively. You're on your own"
+        cmd /c Pause; ""
 
-        Write-Color "--Trying: $Value" -Color 'Cyan'; Write-Color " in $Path\...`n"
-        foreach ($File in ([string[]]$GetName = Get-ChildItem "$Path\" -Recurse -Filter *$Value -Name)) {
-            Out-Null | Out-File "$Path\$File" -Force
+        foreach ($Value in $Extension) {
+            Write-Host "--Trying: $Value in $Path\..."
 
-            switch ([int[]](Get-Item "$Path\$File" | ForEach-Object {[int]($_.length /1kb)})) {
-                0 {Write-Color "O" -Color 'Green'; Write-Color " $Path\$File`n"; $Success++}
-                default {Write-Color "X" -Color 'Red'; Write-Color " $Path\$File`n"; $Failed++}
+            foreach ($File in ([string[]]$GetName = Get-ChildItem "$Path\" -Recurse -Filter *$Value -Name)) {
+                Out-Null | Out-File "$Path\$File" -Force
+
+                switch ([int[]](Get-Item "$Path\$File" | ForEach-Object {[int]($_.length /1kb)})) {
+                    0 {Write-Host "o $Path\$File"; $Success++}
+                    default {Write-Host "- $Path\$File"; $Failed++}
+                }
             }
-        }
-        
-        if ($GetName.length -eq 0) {
-            Write-Color "--Cannot find $Value file(s)`n"
+            
+            if ($GetName.length -eq 0) {
+                Write-Host "--Cannot find $Value file(s)"
+            }; ""
         }
 
-        ""
+        Write-Host "  Success: $Success  //  Failed: $Failed`n"
     }
-
-    Write-Color "  Success: $Success  //  Failed: $Failed`n" -Color 'Cyan'
-    ""
 }
